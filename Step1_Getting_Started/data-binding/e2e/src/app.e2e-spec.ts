1677503d239f912de9ad843c6894906eb2a3021c
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-26 23:43:19 -0500

    Create a new angular project with Bootstrap

--- Step1_Getting_Started/data-binding/e2e/src/app.e2e-spec.ts
+++ Step1_Getting_Started/data-binding/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('create-new-component app is running!');
+    expect(page.getTitleText()).toEqual('data-binding app is running!');
   });
 
   afterEach(async () => {
