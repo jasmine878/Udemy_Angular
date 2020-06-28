a473d365a9504bbe3c422b71f7275a861a2d2404
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-28 17:11:04 -0500

    Implement two way binding

--- Step1_Getting_Started/8_assignment2/src/app/app.module.ts
+++ Step1_Getting_Started/8_assignment2/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
 import { UserNameComponent } from './username/username.component';
@@ -10,7 +11,8 @@ import { UserNameComponent } from './username/username.component';
     UserNameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
