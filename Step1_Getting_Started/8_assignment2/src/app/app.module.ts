ce2245a56309e191031c6eb784a3cb57ad687d09
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-28 16:51:00 -0500

    Create a username component

--- Step1_Getting_Started/8_assignment2/src/app/app.module.ts
+++ Step1_Getting_Started/8_assignment2/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserNameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserNameComponent
   ],
   imports: [
     BrowserModule
