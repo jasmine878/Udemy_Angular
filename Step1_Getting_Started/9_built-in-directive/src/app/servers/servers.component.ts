452cae16ded56291d8a597312032ea94f0f8c3c0
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-29 00:00:08 -0500

    Create a servers component

---
+++ Step1_Getting_Started/9_built-in-directive/src/app/servers/servers.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  serverName: string = "";
+  serverCreationStatus: string = "No server was created"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+}
