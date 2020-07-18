0ac33b3b472b9c731b35ab508d1fbd12ecd8cf99
Tae Hui Chang <jasmine878@yahoo.com>
2020-07-17 23:31:15 -0500

    Create a property through @ViewChild

--- Step5_Components&Databinding/src/app/cockpit/cockpit.component.ts
+++ Step5_Components&Databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,27 +9,45 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', { static: true }) serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // onAddServer(nameInput: HTMLInputElement) {
+  //   console.log(nameInput);
+  //   console.log(nameInput.value);
+
+  //   this.serverCreated.emit({
+  //     serverName: nameInput.value,
+  //     serverContent: this.newServerContent
+  //   });
+  // }
+
   onAddServer(nameInput: HTMLInputElement) {
-    console.log(nameInput);
-    console.log(nameInput.value);
+    console.log(this.serverContentInput);
 
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
+  // onAddBlueprint(nameInput: HTMLInputElement) {
+  //   this.blueprintCreated.emit({
+  //     serverName: nameInput.value,
+  //     serverContent: this.newServerContent
+  //   })
+  // }
+
   onAddBlueprint(nameInput: HTMLInputElement) {
+    this.serverContentInput.nativeElement.value = 'Something';
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
