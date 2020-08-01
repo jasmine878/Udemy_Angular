36597c5e450de80d3da278a2345c44241d6f0232
Tae Hui Chang <jasmine878@yahoo.com>
2020-08-01 17:00:53 -0500

    Create another event emitter

--- Step6_Project-Components&Databinding/1_navigation_w_Event_Binding&ngIf/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Step6_Project-Components&Databinding/1_navigation_w_Event_Binding&ngIf/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,10 +11,15 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe', 'This is simply a test', "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg"),
     new Recipe('A Test Recipe', 'This is simply a test', "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg")
   ];
+  @Output() anotherRecipe = new EventEmitter<Recipe>()
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.anotherRecipe.emit(recipe)
   }
 
 }
