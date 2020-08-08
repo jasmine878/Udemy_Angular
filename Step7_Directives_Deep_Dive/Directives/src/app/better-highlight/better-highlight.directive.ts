eb45fd2ce0e4f387686aaed8f36a90ac30a9c5a3
Tae Hui Chang <jasmine878@yahoo.com>
2020-08-08 14:44:17 -0500

    Create a reactive directive using HostListener

--- Step7_Directives_Deep_Dive/Directives/src/app/better-highlight/better-highlight.directive.ts
+++ Step7_Directives_Deep_Dive/Directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,15 +1,22 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, Host } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
 
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
-  ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement,"background-color", 'blue');
-    this.renderer.setStyle(this.elementRef.nativeElement, "color", 'white');
-  }
+  ngOnInit() {}
 
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, "background-color", 'blue');
+    this.renderer.setStyle(this.elementRef.nativeElement, "color", 'white');
+   }
+
+   @HostListener('mouseleave') mouseleave(eventData: Event) {
+     this.renderer.setStyle(this.elementRef.nativeElement, "background-color", 'transparent');
+     this.renderer.setStyle(this.elementRef.nativeElement, "color", "black");
+   }
 }
