b9bd8dd14fe60c35e3d30f36646a8d3dbb0ff40e
Tae Hui Chang <jasmine878@yahoo.com>
2020-08-23 01:48:45 -0500

    Declare the directive

--- Step8_Project-Directives/1_Building_a_Dropdown_directive/src/app/app.module.ts
+++ Step8_Project-Directives/1_Building_a_Dropdown_directive/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 
@@ -23,6 +24,7 @@ import { FormsModule } from '@angular/forms';
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
