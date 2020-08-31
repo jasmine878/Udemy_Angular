00166b69a106b942ecff5bc3ab2c367e24fba0ae
Tae Hui Chang <jasmine878@yahoo.com>
2020-08-31 02:18:23 -0500

    Remove our event emitter

--- Step9_Services/1_Creating_a_Logging_Service/src/app/new-account/new-account.component.ts
+++ Step9_Services/1_Creating_a_Logging_Service/src/app/new-account/new-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../logging.service';
 
 @Component({
@@ -8,17 +8,9 @@ import { LoggingService } from '../logging.service';
   providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-
-  constructor(private loggingService: LoggingService) {
-
-  }
+  constructor(private loggingService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
     this.loggingService.logStatusChange(accountStatus);
     // console.log('A server status changed, new status: ' + accountStatus);
   }
