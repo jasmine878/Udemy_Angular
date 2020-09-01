f32f3716b0fee9ac473774086803fee29ddbf89d
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-01 01:38:18 -0500

    Add a Service to another Service

--- Step9_Services/2_Injecting_Services_into_Services/src/app/accounts.service.ts
+++ Step9_Services/2_Injecting_Services_into_Services/src/app/accounts.service.ts
@@ -1,3 +1,5 @@
+import { LoggingService } from "./logging.service";
+
 export class AccountsService {
   accounts = [
     {
@@ -14,11 +16,15 @@ export class AccountsService {
     }
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
