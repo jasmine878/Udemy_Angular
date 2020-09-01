13f7dedf56c7f153eb82140d8f5e95eb62ac1cc9
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-01 01:37:01 -0500

    Remove service from Child component

--- Step9_Services/2_Injecting_Services_into_Services/src/app/account/account.component.ts
+++ Step9_Services/2_Injecting_Services_into_Services/src/app/account/account.component.ts
@@ -6,7 +6,7 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
@@ -17,7 +17,7 @@ export class AccountComponent {
 
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
+    // this.loggingService.logStatusChange(status);
     // console.log('A server status changed, new status: ' + status);
   }
 }
