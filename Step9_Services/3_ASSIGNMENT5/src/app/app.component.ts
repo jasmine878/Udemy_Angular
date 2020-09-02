7b9e235b048ebaed58db084c37d9d6f9eba70da5
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-02 16:58:55 -0500

    Inject the Service at the parent level

--- Step9_Services/3_ASSIGNMENT5/src/app/app.component.ts
+++ Step9_Services/3_ASSIGNMENT5/src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './Users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UsersService]
 })
-export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
 
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
+export class AppComponent implements OnInit {
+  activeUsers: string[];
+  inactiveUsers: string[];
 
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.activeUsers = this.usersService.activeUsers;
+    this.inactiveUsers = this.usersService.inactiveUsers;
   }
 }
