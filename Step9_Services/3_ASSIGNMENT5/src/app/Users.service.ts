f023c958e220289bb944facc9b8346f11a0c9490
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-02 23:40:15 -0500

    Inject the Service into another Service

--- Step9_Services/3_ASSIGNMENT5/src/app/Users.service.ts
+++ Step9_Services/3_ASSIGNMENT5/src/app/Users.service.ts
@@ -1,14 +1,23 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./Logging.service";
+
+@Injectable()
+
 export class UsersService {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
+  constructor(private loggingService: LoggingService) {}
+
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.loggingService.addInactive();
   }
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.loggingService.addActive();
   }
 }
