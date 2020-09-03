403227b04b0add0435585c6f1d4051b6dfa0a05a
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-02 23:39:59 -0500

    Add the Service to the Provider

--- Step9_Services/3_ASSIGNMENT5/src/app/app.module.ts
+++ Step9_Services/3_ASSIGNMENT5/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { LoggingService } from './Logging.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +16,7 @@ import { InactiveUsersComponent } from './inactive-users/inactive-users.componen
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
