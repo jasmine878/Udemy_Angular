ecaf7f12640f539945e026599805ab530f10e1bf
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-04 01:27:37 -0500

    Move Recipe instances to the Service

---
+++ Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipes/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg"),
+    new Recipe('Another Test Recipe', 'This is simply a test', "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg")
+  ];
+
+  selectedRecipe: Recipe;
+
+  updateRecipe(recipe: Recipe) {
+    this.selectedRecipe = recipe;
+  }
+}
