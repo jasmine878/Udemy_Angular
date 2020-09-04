2773cf95f3c0991abbc0575e6d40ed7f3582f944
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-04 01:28:11 -0500

    Make recipes property private

--- Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipes/recipe.service.ts
+++ Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipes/recipe.service.ts
@@ -1,12 +1,16 @@
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
-  recipes: Recipe[] = [
+  private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg"),
     new Recipe('Another Test Recipe', 'This is simply a test', "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg")
   ];
 
   selectedRecipe: Recipe;
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
   updateRecipe(recipe: Recipe) {
     this.selectedRecipe = recipe;
