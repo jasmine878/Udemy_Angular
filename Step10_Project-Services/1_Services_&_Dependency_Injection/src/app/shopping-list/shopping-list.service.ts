b2f7a6ff05011eb8e421e1790a707f3eaa516bdc
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-04 02:29:52 -0500

    Make ingredients array private

--- Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/shopping-list/shopping-list.service.ts
+++ Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,14 @@
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppinglistService {
-  ingredients: Ingredient[] = [
+  private ingredients: Ingredient[] = [
     new Ingredient("Apples", 5),
     new Ingredient("Tomatoes", 10),
   ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
 
   addItem(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
