05088b5900d5804985e8147a841da613791d5902
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-03 22:32:49 -0500

    Create a service

---
+++ Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/shopping-list.service.ts
@@ -0,0 +1,12 @@
+import { Ingredient } from './shared/ingredient.model';
+
+export class ShoppinglistService {
+  ingredients: Ingredient[] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatoes", 10),
+  ];
+
+  addItem(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+}
