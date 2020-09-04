bc086ffb38597b40c324cdde9aa6a4c85e66d76d
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-04 02:18:26 -0500

    Refactor Recipe List Component

--- Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/app.component.ts
+++ Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { ShoppinglistService } from './shopping-list.service';
-import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ShoppinglistService, RecipeService]
+  providers: []
 })
 export class AppComponent {
   displayContent: string = 'recipe';
--- Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -15,9 +15,5 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    // this.anotherRecipe.emit(recipe)
   }
 }
