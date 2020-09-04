4efbe49eebe2bd56fe6591303a2d2b679999de03
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-04 02:16:30 -0500

    Add an Event Emitter to the Recipe Service

--- Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipe.service.ts
+++
@@ -1,9 +0,0 @@
-import { Recipe } from './recipes/recipe.model';
-
-export class RecipeService {
-  selectedRecipe: Recipe;
-
-  updateRecipe(recipe) {
-    this.selectedRecipe = recipe;
-  }
-}
--- Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipes/recipe.service.ts
+++ Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipes/recipe.service.ts
@@ -1,18 +1,15 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg"),
     new Recipe('Another Test Recipe', 'This is simply a test', "https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg")
   ];
 
-  selectedRecipe: Recipe;
-
   getRecipes() {
     return this.recipes.slice();
-  }
-
-  updateRecipe(recipe: Recipe) {
-    this.selectedRecipe = recipe;
   }
 }
