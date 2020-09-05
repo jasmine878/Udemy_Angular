d9e308ef616f59bcb30914efeff6e25212b6afcc
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-05 17:30:45 -0500

    Add our ingredients to our ShoppingList Service

--- Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ Step10_Project-Services/1_Services_&_Dependency_Injection/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent implements OnInit {
+export class RecipeDetailComponent {
   @Input() recipeDisplay: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {
+  addShoppingList() {
+    this.recipeService.addToShoppingList(this.recipeDisplay.ingredients);
   }
-
 }
