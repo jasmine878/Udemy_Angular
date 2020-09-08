85591e8692ac8c0ef602bd82eadd6fa6a6e9d2dc
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-08 00:25:47 -0500

    Register our routes in the Angular application

--- Step11_Changing_Pages_w_Routing/1_Routing/src/app/app.module.ts
+++ Step11_Changing_Pages_w_Routing/1_Routing/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +11,6 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -32,8 +31,10 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
