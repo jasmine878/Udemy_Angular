8cbebd2aee9cf47d714821c95f0734a4ef15caa4
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-20 01:34:53 -0500

    Programmatically add query parameters

--- Step11_Changing_Pages_w_Routing/2_Routing_w_parameters/src/app/home/home.component.ts
+++ Step11_Changing_Pages_w_Routing/2_Routing_w_parameters/src/app/home/home.component.ts
@@ -13,8 +13,8 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServer(id: number) {
     //complex calculations here
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 }
