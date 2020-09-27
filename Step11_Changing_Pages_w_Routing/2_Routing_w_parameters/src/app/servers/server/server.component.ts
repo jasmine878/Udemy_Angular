da14e8cd9e9930d862f20d5519fb6a0cd002c129
Tae Hui Chang <jasmine878@yahoo.com>
2020-09-26 21:43:40 -0500

    Convert our parameter to a number

--- Step11_Changing_Pages_w_Routing/2_Routing_w_parameters/src/app/servers/server/server.component.ts
+++ Step11_Changing_Pages_w_Routing/2_Routing_w_parameters/src/app/servers/server/server.component.ts
@@ -14,12 +14,12 @@ export class ServerComponent implements OnInit {
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params
       .subscribe(
         (params: Params) => {
-          this.server = this.serversService.getServer(params["id"]);
+          this.server = this.serversService.getServer(+params["id"]);
         }
       );
   }
