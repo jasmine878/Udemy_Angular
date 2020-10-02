432a1811732f84053aa309cd69074c6ac80da5ba
Tae Hui Chang <jasmine878@yahoo.com>
2020-10-02 14:14:08 -0500

    Create an allowEdit property

--- Step11_Changing_Pages_w_Routing/3_Child_(nested)_Routing/src/app/servers/edit-server/edit-server.component.ts
+++ Step11_Changing_Pages_w_Routing/3_Child_(nested)_Routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute) { }
